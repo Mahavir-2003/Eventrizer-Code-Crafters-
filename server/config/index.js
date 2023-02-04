42c63bc0ff4bd610db7a6dc075c440bc8d7673b9
Mahavir-2003 <93638988+Mahavir-2003@users.noreply.github.com>
2023-02-05 05:15:10 +0530

    Delete server directory

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const {
-    APP_PORT,
-    DB_URL
-} = process.env;
--- server/models/index.js
+++
@@ -1,2 +0,0 @@
-export {default as Organizer} from "./organizer"
-export {default as Vender} from "./vender"
