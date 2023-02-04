14945c2bee5100152a7d848bdf3739bd36b4661c
Mahavir-2003 <93638988+Mahavir-2003@users.noreply.github.com>
2023-02-05 00:48:43 +0530

    Add files via upload

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+import Logo from '../../SVG/Group 1.svg'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={Logo} alt="Logo" />
+      <Link className="addEvent" to="/organizerLogin">
+      Add Event
+      </Link>
+    </div>
+  )
+}
+
+export default Header
